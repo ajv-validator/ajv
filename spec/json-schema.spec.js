2368b95d5678d1d17db9a4a4818dae1de32284c3
Evgeny Poberezkin <e.poberezkin@me.com>
2015-06-19 11:39:48 +0100

    refactored json-schema-test into separate npm module

--- spec/json-schema.spec.js
+++ spec/json-schema.spec.js
@@ -1,33 +1,11 @@
 'use strict';
 
-var glob = require('glob')
-  , path = require('path')
-  , assert = require('assert');
-
-var ONLY_FILES, SKIP_FILES;
-ONLY_FILES = [
-// 'type', 'not', 'allOf', 'anyOf', 'oneOf', 'enum',
-// 'maximum', 'minimum', 'multipleOf', 'maxLength', 'minLength', 'pattern',
-// 'properties', 'patternProperties', 'additionalProperties',
-// 'dependencies', 'required',
-// 'maxProperties', 'minProperties', 'maxItems', 'minItems',
-// 'items', 'additionalItems', 'uniqueItems',
-// 'optional/format', 'optional/bignum',
-// 'ref', 'refRemote', 'definitions',
-// 'schemas/complex', 'schemas/basic', 'schemas/advanced',
-];
-
-SKIP_FILES = [
-  'optional/zeroTerminatedFloats',
-  'schemas/complex'
-];
-
-var DEBUG = false;
+var jsonSchemaTest = require('json-schema-test');
 
 
 var Ajv = require('../lib/ajv')
-  , ajv = Ajv({ beautify: true, _debug: DEBUG })
-  , fullAjv = Ajv({ allErrors: true, verbose: true, format: 'full', beautify: true, _debug: DEBUG });
+  , ajv = Ajv({ beautify: true, _debug: false })
+  , fullAjv = Ajv({ allErrors: true, verbose: true, format: 'full', beautify: true, _debug: false });
 
 var remoteRefs = {
     // for JSON-Schema-Test-Suite
@@ -44,75 +22,40 @@ var remoteRefsWithIds = [ // order is important
   require('./remotes/buu.json'),
 ];
 
-for (var id in remoteRefs) {
-  ajv.addSchema(remoteRefs[id], id);
-  fullAjv.addSchema(remoteRefs[id], id);
-}
-
-ajv.addSchema(remoteRefsWithIds);
-fullAjv.addSchema(remoteRefsWithIds);
+addRemoteRefs();
 
 
-describe('Schema validation tests', function() {
-  addTests('JSON-Schema tests draft4', './JSON-Schema-Test-Suite/tests/draft4/{**/,}*.json');
-  addTests('Advanced schema tests', './tests/{**/,}*.json');
+jsonSchemaTest([ ajv, fullAjv ], {
+  suites: {
+    'JSON-Schema tests draft4': './JSON-Schema-Test-Suite/tests/draft4/{**/,}*.json',
+    'Advanced schema tests': './tests/{**/,}*.json'
+  },
+  only: [
+    // 'type', 'not', 'allOf', 'anyOf', 'oneOf', 'enum',
+    // 'maximum', 'minimum', 'multipleOf', 'maxLength', 'minLength', 'pattern',
+    // 'properties', 'patternProperties', 'additionalProperties',
+    // 'dependencies', 'required',
+    // 'maxProperties', 'minProperties', 'maxItems', 'minItems',
+    // 'items', 'additionalItems', 'uniqueItems',
+    // 'optional/format', 'optional/bignum',
+    // 'ref', 'refRemote', 'definitions',
+    // 'schemas/complex', 'schemas/basic', 'schemas/advanced',
+  ],
+  skip: [
+    'optional/zeroTerminatedFloats',
+    'schemas/complex'
+  ],
+  cwd: __dirname,
+  hideFolder: 'draft4/'
 });
 
 
-function addTests(description, testsPath) {
-  describe(description, function() {
-    var files = getTestFiles(testsPath);
+function addRemoteRefs() {
+  for (var id in remoteRefs) {
+    ajv.addSchema(remoteRefs[id], id);
+    fullAjv.addSchema(remoteRefs[id], id);
+  }
 
-    files.forEach(function (file) {
-      var filter = {
-        skip: SKIP_FILES && SKIP_FILES.indexOf(file.name) >= 0,
-        only: ONLY_FILES && ONLY_FILES.indexOf(file.name) >= 0
-      }
-
-      skipOrOnly(filter, describe)(file.name, function() {
-        var testSets = require(file.path);
-        testSets.forEach(function (testSet) {
-          skipOrOnly(testSet, describe)(testSet.description, function() {
-            var validate, fullValidate;
-            before(function() {
-              validate = ajv.compile(testSet.schema);
-              // console.log('validate', validate.toString());
-              fullValidate = fullAjv.compile(testSet.schema);
-            });
-
-            testSet.tests.forEach(function (test) {
-              skipOrOnly(test, it)(test.description, function() {
-                doTest(validate);
-                doTest(fullValidate);
-              });
-
-              function doTest(validate) {
-                var valid = validate(test.data);
-                if (valid !== test.valid) console.log('result', valid, test.valid, validate.errors);
-                assert.equal(valid, test.valid);
-                if (valid) assert(validate.errors === null);
-                else assert(validate.errors.length > 0);
-              }
-            });
-          });
-        });
-      });
-    });
-  });
-}
-
-
-function skipOrOnly(filter, func) {
-  return filter.only ? func.only : filter.skip ? func.skip : func;
-}
-
-
-function getTestFiles(testsPath) {
-  var files = glob.sync(testsPath, { cwd: __dirname });
-  return files.map(function (file) {
-    var match = file.match(/(\w+\/)\w+\.json/)
-    var folder = match ? match[1] : '';
-    if (folder == 'draft4/') folder = '';
-    return { path: file, name: folder + path.basename(file, '.json') };
-  });
+  ajv.addSchema(remoteRefsWithIds);
+  fullAjv.addSchema(remoteRefsWithIds);
 }
