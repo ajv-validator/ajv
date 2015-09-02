bec34c5ba51b7a0e2a594e260a280ee1f5eb6a85
Evgeny Poberezkin <e.poberezkin@me.com>
2015-09-02 18:20:43 +0100

    error thrown in case of missing reference has properties missingRef and missingSchema, related to #43, mulesoft-labs/osprey-mock-service#11

--- spec/resolve.spec.js
+++ spec/resolve.spec.js
@@ -72,73 +72,126 @@ describe('resolve', function () {
         });
       });
     });
+  });
 
 
-    describe('inline referenced schemas without refs in them', function() {
-      var schemas = [
-        { id: 'http://e.com/obj.json#',
-          properties: { a: { $ref: 'int.json#' } } },
-        { id: 'http://e.com/int.json#',
-          type: 'integer', minimum: 2, maximum: 4 },
-        { id: 'http://e.com/obj1.json#',
-          definitions: { int: { type: 'integer', minimum: 2, maximum: 4 } },
-          properties: { a: { $ref: '#/definitions/int' } } },
-        { id: 'http://e.com/list.json#',
-          items: { $ref: 'obj.json#' } }
-      ];
-
-      it('by default should inline schema if it doesn\'t contain refs', function() {
-        var ajv = Ajv({ schemas: schemas });
-        testSchemas(ajv, true);
-      });
-
-
-      it('should NOT inline schema if option inlineRefs == false', function() {
-        var ajv = Ajv({ schemas: schemas, inlineRefs: false });
-        testSchemas(ajv, false);
-      });
-
-
-      it('should inline schema if option inlineRefs is bigger than number of keys in referenced schema', function() {
-        var ajv = Ajv({ schemas: schemas, inlineRefs: 3 });
-        testSchemas(ajv, true);
-      });
-
-
-      it('should NOT inline schema if option inlineRefs is less than number of keys in referenced schema', function() {
-        var ajv = Ajv({ schemas: schemas, inlineRefs: 2 });
-        testSchemas(ajv, false);
-      });
-
-
-      function testSchemas(ajv, expectedInlined) {
-        var v1 = ajv.getSchema('http://e.com/obj.json')
-          , v2 = ajv.getSchema('http://e.com/obj1.json')
-          , vl = ajv.getSchema('http://e.com/list.json');
-        testObjSchema(v1);
-        testObjSchema(v2);
-        testListSchema(vl);
-        testInlined(v1, expectedInlined);
-        testInlined(v2, expectedInlined);
-        testInlined(vl, false);
-      }
-
-      function testObjSchema(validate) {
-        validate({a:3}) .should.equal(true);
-        validate({a:1}) .should.equal(false);
-        validate({a:5}) .should.equal(false);
-      }
-
-      function testListSchema(validate) {
-        validate([{a:3}]) .should.equal(true);
-        validate([{a:1}]) .should.equal(false);
-        validate([{a:5}]) .should.equal(false);
-      }
-
-      function testInlined(validate, expectedInlined) {
-        var inlined = !(/refVal/.test(validate.toString()));
-        inlined .should.equal(expectedInlined);
-      }
+  describe('missing schema error', function() {
+    it('should contain missingRef and missingSchema', function (done) {
+      testMissingSchemaError({
+        baseId: 'http://example.com/1.json',
+        ref: 'http://another.com/int.json',
+        expectedMissingRef: 'http://another.com/int.json',
+        expectedMissingSchema: 'http://another.com/int.json'
+      }, done);
     });
+
+    it('should resolve missingRef and missingSchema relative to base id', function (done) {
+      testMissingSchemaError({
+        baseId: 'http://example.com/folder/1.json',
+        ref: 'int.json',
+        expectedMissingRef: 'http://example.com/folder/int.json',
+        expectedMissingSchema: 'http://example.com/folder/int.json'
+      }, done);
+    });
+
+    it('should resolve missingRef and missingSchema relative to base id from root', function (done) {
+      testMissingSchemaError({
+        baseId: 'http://example.com/folder/1.json',
+        ref: '/int.json',
+        expectedMissingRef: 'http://example.com/int.json',
+        expectedMissingSchema: 'http://example.com/int.json'
+      }, done);
+    });
+
+    it('missingRef should and missingSchema should NOT include hash fragment', function (done) {
+      testMissingSchemaError({
+        baseId: 'http://example.com/1.json',
+        ref: 'int.json#/definitions/positive',
+        expectedMissingRef: 'http://example.com/int.json#/definitions/positive',
+        expectedMissingSchema: 'http://example.com/int.json'
+      }, done);
+    });
+
+
+    function testMissingSchemaError(opts, done) {
+      try {
+        ajv.compile({
+          "id": opts.baseId,
+          "properties": { "a": { "$ref": opts.ref } }
+        });
+      } catch(e) {
+        e.missingRef .should.equal(opts.expectedMissingRef);
+        e.missingSchema .should.equal(opts.expectedMissingSchema);
+        done();
+      }
+    }
+  });
+
+
+  describe('inline referenced schemas without refs in them', function() {
+    var schemas = [
+      { id: 'http://e.com/obj.json#',
+        properties: { a: { $ref: 'int.json#' } } },
+      { id: 'http://e.com/int.json#',
+        type: 'integer', minimum: 2, maximum: 4 },
+      { id: 'http://e.com/obj1.json#',
+        definitions: { int: { type: 'integer', minimum: 2, maximum: 4 } },
+        properties: { a: { $ref: '#/definitions/int' } } },
+      { id: 'http://e.com/list.json#',
+        items: { $ref: 'obj.json#' } }
+    ];
+
+    it('by default should inline schema if it doesn\'t contain refs', function() {
+      var ajv = Ajv({ schemas: schemas });
+      testSchemas(ajv, true);
+    });
+
+
+    it('should NOT inline schema if option inlineRefs == false', function() {
+      var ajv = Ajv({ schemas: schemas, inlineRefs: false });
+      testSchemas(ajv, false);
+    });
+
+
+    it('should inline schema if option inlineRefs is bigger than number of keys in referenced schema', function() {
+      var ajv = Ajv({ schemas: schemas, inlineRefs: 3 });
+      testSchemas(ajv, true);
+    });
+
+
+    it('should NOT inline schema if option inlineRefs is less than number of keys in referenced schema', function() {
+      var ajv = Ajv({ schemas: schemas, inlineRefs: 2 });
+      testSchemas(ajv, false);
+    });
+
+
+    function testSchemas(ajv, expectedInlined) {
+      var v1 = ajv.getSchema('http://e.com/obj.json')
+        , v2 = ajv.getSchema('http://e.com/obj1.json')
+        , vl = ajv.getSchema('http://e.com/list.json');
+      testObjSchema(v1);
+      testObjSchema(v2);
+      testListSchema(vl);
+      testInlined(v1, expectedInlined);
+      testInlined(v2, expectedInlined);
+      testInlined(vl, false);
+    }
+
+    function testObjSchema(validate) {
+      validate({a:3}) .should.equal(true);
+      validate({a:1}) .should.equal(false);
+      validate({a:5}) .should.equal(false);
+    }
+
+    function testListSchema(validate) {
+      validate([{a:3}]) .should.equal(true);
+      validate([{a:1}]) .should.equal(false);
+      validate([{a:5}]) .should.equal(false);
+    }
+
+    function testInlined(validate, expectedInlined) {
+      var inlined = !(/refVal/.test(validate.toString()));
+      inlined .should.equal(expectedInlined);
+    }
   });
 });
