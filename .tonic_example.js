8b98518b1068c670f20ec304f6c25c9b8d73c81c
Evgeny Poberezkin <e.poberezkin@me.com>
2015-12-13 23:24:48 +0000

    fixed jshint script

--- .tonic_example.js
+++ .tonic_example.js
@@ -2,10 +2,10 @@ var Ajv = require('ajv');
 var ajv = Ajv({allErrors: true});
 
 var schema = {
-    "properties": {
-        "foo": { "type": "string" },
-        "bar": { "type": "number", "maximum": 3 }
-    }
+  "properties": {
+    "foo": { "type": "string" },
+    "bar": { "type": "number", "maximum": 3 }
+  }
 };
 
 var validate = ajv.compile(schema);
@@ -14,7 +14,7 @@ test({"foo": "abc", "bar": 2});
 test({"foo": 2, "bar": 4});
 
 function test(data) {
-    var valid = validate(data);
-    if (valid) console.log('Valid!');
-    else console.log('Invalid: ' + ajv.errorsText(validate.errors));
+  var valid = validate(data);
+  if (valid) console.log('Valid!');
+  else console.log('Invalid: ' + ajv.errorsText(validate.errors));
 }
