8b8ab4b086bb972fa8b168ac9f19a108f421479d
Evgeny Poberezkin <e.poberezkin@me.com>
2016-01-26 13:18:50 +0000

    fixed browser tests with the new brfs version

--- spec/custom_rules/index.js
+++ spec/custom_rules/index.js
@@ -4,6 +4,6 @@ var fs = require('fs')
   , doT = require('dot');
 
 module.exports = {
-  range:           doT.compile(fs.readFileSync(__dirname + '/range.jst')),
-  rangeWithErrors: doT.compile(fs.readFileSync(__dirname + '/range_with_errors.jst'))
+  range:           doT.compile(fs.readFileSync(__dirname + '/range.jst', 'utf8')),
+  rangeWithErrors: doT.compile(fs.readFileSync(__dirname + '/range_with_errors.jst', 'utf8'))
 };
