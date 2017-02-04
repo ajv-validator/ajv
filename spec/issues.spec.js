f5bcd5735a4ce566e3830f67bd3272d6df4f2694
Evgeny Poberezkin <e.poberezkin@me.com>
2017-02-04 22:15:23 +0000

    fix: code clean-up, closes #388

--- spec/issues.spec.js
+++ spec/issues.spec.js
@@ -524,3 +524,27 @@ describe('issue #342, support uniqueItems with some non-JSON objects', function(
     validate([{foo: undefined}, {foo: undefined}]) .should.equal(false);
   });
 });
+
+
+describe('issue #388, code clean-up not working', function() {
+  it('should remove assignement to rootData if it is not used', function() {
+    var ajv = new Ajv;
+    var validate = ajv.compile({
+      type: 'object',
+      properties: {
+        foo: { type: 'string' }
+      }
+    });
+    var code = validate.toString();
+    code.match(/rootData/g).length .should.equal(1);
+  });
+
+  it('should remove assignement to errors if they are not used', function() {
+    var ajv = new Ajv;
+    var validate = ajv.compile({
+      type: 'object'
+    });
+    var code = validate.toString();
+    should.equal(code.match(/[^\.]errors|vErrors/g), null);
+  });
+});
