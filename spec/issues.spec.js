f60fedb4e905bfd6c8ca545c6687dc2b83f3bffd
Evgeny Poberezkin <e.poberezkin@me.com>
2017-05-13 23:17:29 +0100

    feat: validate types before other keywords, #485

--- spec/issues.spec.js
+++ spec/issues.spec.js
@@ -548,3 +548,31 @@ describe('issue #388, code clean-up not working', function() {
     should.equal(code.match(/[^\.]errors|vErrors/g), null);
   });
 });
+
+
+describe('issue #485, order of type validation', function() {
+  it('should validate types befor keywords', function() {
+    var ajv = new Ajv({allErrors: true});
+    var validate = ajv.compile({
+      type: ['integer', 'string'],
+      required: ['foo'],
+      minimum: 2
+    });
+
+    validate(2) .should.equal(true);
+    validate('foo') .should.equal(true);
+
+    validate(1.5) .should.equal(false);
+    checkErrors(['type', 'minimum']);
+
+    validate({}) .should.equal(false);
+    checkErrors(['type', 'required']);
+
+    function checkErrors(expectedErrs) {
+      validate.errors .should.have.length(expectedErrs.length);
+      expectedErrs.forEach(function (keyword, i) {
+        validate.errors[i].keyword .should.equal(keyword);
+      });
+    }
+  });
+});
