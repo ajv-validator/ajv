6a2225b0434dd61339ef7c357b18e8ce6ddf8cde
Evgeny Poberezkin <e.poberezkin@me.com>
2017-07-10 15:46:50 +0100

    fix: same missing $ref used more then once throws exception with missingRefs: false, fixes #533

--- spec/issues.spec.js
+++ spec/issues.spec.js
@@ -600,3 +600,28 @@ describe('issue #521, incorrect warning with "id" property', function() {
     }
   });
 });
+
+
+describe('issue #533, throwing missing ref exception with option missingRefs: "ignore"', function() {
+  var schema = {
+    "type": "object",
+    "properties": {
+      "foo": {"$ref": "#/definitions/missing"},
+      "bar": {"$ref": "#/definitions/missing"}
+    }
+  };
+
+  it('should pass validation without throwing exception', function() {
+    var ajv = new Ajv({missingRefs: 'ignore'});
+    var validate = ajv.compile(schema);
+    validate({foo: 'anything'}) .should.equal(true);
+    validate({foo: 'anything', bar: 'whatever'}) .should.equal(true);
+  });
+
+  it('should throw exception during schema compilation with option missingRefs: true', function() {
+    var ajv = new Ajv;
+    should.throw(function() {
+      ajv.compile(schema);
+    });
+  });
+});
