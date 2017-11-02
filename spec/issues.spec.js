1d44f1451a960f8e1163f3ad0443bcc86733a895
Brendan Abbott <brendan@bloodbone.ws>
2017-11-03 02:18:41 +1000

    Add leap year support for date-time/date formats in full mode

--- spec/issues.spec.js
+++ spec/issues.spec.js
@@ -625,3 +625,27 @@ describe('issue #533, throwing missing ref exception with option missingRefs: "i
     });
   });
 });
+
+describe('full date format validation should understand leap years', function () {
+  it('should handle year leaps as date-time', function() {
+    var ajv = new Ajv({ format: 'full' });
+
+    var schema = { format: 'date-time' };
+    var validDateTime = '2016-02-29T00:00:00Z';
+    var invalidDateTime = '2017-02-29T00:00:00Z';
+
+    ajv.validate(schema, validDateTime) .should.equal(true);
+    ajv.validate(schema, invalidDateTime) .should.equal(false);
+  });
+
+  it('should handle year leaps as date', function() {
+    var ajv = new Ajv({ format: 'full' });
+
+    var schema = { format: 'date' };
+    var validDate = '2016-02-29';
+    var invalidDate = '2017-02-29';
+
+    ajv.validate(schema, validDate) .should.equal(true);
+    ajv.validate(schema, invalidDate) .should.equal(false);
+  });
+});
