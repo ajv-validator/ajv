27855e1b72e5830e23f7719406cfdb8be5dcd024
Brendan Abbott <brendan@bloodbone.ws>
2017-11-03 21:35:38 +1000

    Fix logic error for non leap years and months, add more tests

--- spec/issues.spec.js
+++ spec/issues.spec.js
@@ -627,6 +627,24 @@ describe('issue #533, throwing missing ref exception with option missingRefs: "i
 });
 
 describe('full date format validation should understand leap years', function () {
+  it('should handle non leap year affected dates with date-time', function() {
+    var ajv = new Ajv({ format: 'full' });
+
+    var schema = { format: 'date-time' };
+    var validDateTime = '2016-01-31T00:00:00Z';
+
+    ajv.validate(schema, validDateTime).should.equal(true);
+  });
+
+  it('should handle non leap year affected dates with date', function () {
+    var ajv = new Ajv({ format: 'full' });
+
+    var schema = { format: 'date' };
+    var validDate = '2016-11-30';
+
+    ajv.validate(schema, validDate).should.equal(true);
+  });
+
   it('should handle year leaps as date-time', function() {
     var ajv = new Ajv({ format: 'full' });
 
