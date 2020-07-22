b1bb27e28026c023d8f786b421c01b18f99b6de2
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-07-22 17:19:59 +0100

    feat: remove schemaId option (and support for draft-04 "id" in schemas)

--- spec/issues/521_wrong_warning_id_property.spec.js
+++ spec/issues/521_wrong_warning_id_property.spec.js
@@ -5,7 +5,7 @@ require("../chai").should()
 
 describe('issue #521, incorrect warning with "id" property', function () {
   it("should not log warning", function () {
-    var ajv = new Ajv({schemaId: "$id"})
+    var ajv = new Ajv()
     var consoleWarn = console.warn
     console.warn = function () {
       throw new Error("should not log warning")
--- spec/options/schemaId.spec.js
+++ spec/options/schemaId.spec.js
@@ -3,67 +3,30 @@
 var Ajv = require("../ajv")
 var should = require("../chai").should()
 
-describe("schemaId option", function () {
-  describe('= "$id" (default)', function () {
-    it("should use $id and ignore id", function () {
-      test(new Ajv())
-      test(new Ajv({schemaId: "$id"}))
-
-      function test(ajv) {
-        ajv.addSchema({$id: "mySchema1", type: "string"})
-        var validate = ajv.getSchema("mySchema1")
-        validate("foo").should.equal(true)
-        validate(1).should.equal(false)
-
-        validate = ajv.compile({id: "mySchema2", type: "string"})
-        should.not.exist(ajv.getSchema("mySchema2"))
-      }
+describe("removed schemaId option", function () {
+  it('should throw error if schemaId option is used and it is not equal to "$id"', function () {
+    new Ajv()
+    new Ajv({schemaId: "$id"})
+    should.throw(function () {
+      new Ajv({schemaId: "id"})
+    })
+    should.throw(function () {
+      new Ajv({schemaId: "auto"})
     })
   })
 
-  describe('= "id"', function () {
-    it("should use id and ignore $id", function () {
-      var ajv = new Ajv({schemaId: "id", meta: false})
+  it("should use $id and ignore id", function () {
+    test(new Ajv())
+    test(new Ajv({schemaId: "$id"}))
 
-      ajv.addSchema({id: "mySchema1", type: "string"})
-      var validate = ajv.getSchema("mySchema1")
-      validate("foo").should.equal(true)
-      validate(1).should.equal(false)
-
-      validate = ajv.compile({$id: "mySchema2", type: "string"})
-      should.not.exist(ajv.getSchema("mySchema2"))
-    })
-  })
-
-  describe('= "auto"', function () {
-    it("should use both id and $id", function () {
-      var ajv = new Ajv({schemaId: "auto"})
-
+    function test(ajv) {
       ajv.addSchema({$id: "mySchema1", type: "string"})
       var validate = ajv.getSchema("mySchema1")
       validate("foo").should.equal(true)
       validate(1).should.equal(false)
 
-      ajv.addSchema({id: "mySchema2", type: "string"})
-      validate = ajv.getSchema("mySchema2")
-      validate("foo").should.equal(true)
-      validate(1).should.equal(false)
-    })
-
-    it("should throw if both id and $id are available and different", function () {
-      var ajv = new Ajv({schemaId: "auto"})
-
-      ajv.compile({
-        id: "mySchema",
-        $id: "mySchema",
-      })
-
-      should.throw(function () {
-        ajv.compile({
-          id: "mySchema1",
-          $id: "mySchema2",
-        })
-      })
-    })
+      validate = ajv.compile({id: "mySchema2", type: "string"})
+      should.not.exist(ajv.getSchema("mySchema2"))
+    }
   })
 })
