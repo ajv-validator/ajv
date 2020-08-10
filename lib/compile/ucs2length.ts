acfd737c3130837af32be10d569493f7ba403428
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-08-10 22:43:19 +0100

    refactor: ucs2length to typescript

--- lib/compile/ucs2length.ts
+++ lib/compile/ucs2length.ts
@@ -2,18 +2,18 @@
 // https://github.com/bestiejs/punycode.js - punycode.ucs2.decode
 module.exports = ucs2length
 
-export default function ucs2length(str) {
-  var length = 0,
-    len = str.length,
-    pos = 0,
-    value
+export default function ucs2length(str: string): number {
+  let length = 0
+  let len = str.length
+  let pos = 0
+  let value: number
   while (pos < len) {
     length++
     value = str.charCodeAt(pos++)
     if (value >= 0xd800 && value <= 0xdbff && pos < len) {
       // high surrogate, and there is a next character
       value = str.charCodeAt(pos)
-      if ((value & 0xfc00) == 0xdc00) pos++ // low surrogate
+      if ((value & 0xfc00) === 0xdc00) pos++ // low surrogate
     }
   }
   return length
