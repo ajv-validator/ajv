8daaf7da67f0527388f9d769d2e6b2b94933571c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-09-11 16:33:58 +0100

    tests: update JSON-Schema-Test-Suite tests

--- spec/json-schema.spec.ts
+++ spec/json-schema.spec.ts
@@ -7,36 +7,40 @@ import {afterError, afterEach} from "./after_test"
 const remoteRefs = {
   "http://localhost:1234/integer.json": require("./JSON-Schema-Test-Suite/remotes/integer.json"),
   "http://localhost:1234/subSchemas.json": require("./JSON-Schema-Test-Suite/remotes/subSchemas.json"),
-  "http://localhost:1234/folder/folderInteger.json": require("./JSON-Schema-Test-Suite/remotes/folder/folderInteger.json"),
+  "http://localhost:1234/baseUriChange/folderInteger.json": require("./JSON-Schema-Test-Suite/remotes/baseUriChange/folderInteger.json"),
+  "http://localhost:1234/baseUriChangeFolder/folderInteger.json": require("./JSON-Schema-Test-Suite/remotes/baseUriChangeFolder/folderInteger.json"),
+  "http://localhost:1234/baseUriChangeFolderInSubschema/folderInteger.json": require("./JSON-Schema-Test-Suite/remotes/baseUriChangeFolderInSubschema/folderInteger.json"),
   "http://localhost:1234/name.json": require("./JSON-Schema-Test-Suite/remotes/name.json"),
 }
 
+const SKIP6 = [
+  "optional/ecmascript-regex", // TODO only format needs to be skipped, too much is skipped here
+  "optional/non-bmp-regex",
+  "format",
+  "optional/format/date",
+  "optional/format/date-time",
+  "optional/format/email",
+  "optional/format/hostname",
+  "optional/format/ipv4",
+  "optional/format/ipv6",
+  "optional/format/json-pointer",
+  "optional/format/uri",
+  "optional/format/uri-reference",
+  "optional/format/uri-template",
+]
+
 const SKIP = {
-  6: [
-    "optional/ecmascript-regex", // TODO only format needs to be skipped, too much is skipped here
-    "optional/format",
-  ],
-  7: [
-    "optional/ecmascript-regex", // TODO only format needs to be skipped, too much is skipped here
+  6: SKIP6,
+  7: SKIP6.concat([
     "optional/content",
-    "optional/format/date",
-    "optional/format/date-time",
-    "optional/format/email",
-    "optional/format/hostname",
     "optional/format/idn-email",
     "optional/format/idn-hostname",
-    "optional/format/ipv4",
-    "optional/format/ipv6",
     "optional/format/iri",
     "optional/format/iri-reference",
-    "optional/format/json-pointer",
     "optional/format/regex",
     "optional/format/relative-json-pointer",
     "optional/format/time",
-    "optional/format/uri",
-    "optional/format/uri-reference",
-    "optional/format/uri-template",
-  ],
+  ]),
 }
 
 runTest(getAjvInstances(options, {meta: false, strict: false}), 6, require("./_json/draft6"))
