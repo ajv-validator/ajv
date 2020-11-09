6937eb32b6474cfeaccf61a7a33f0cf07cc94dfe
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-11-09 07:36:27 +0000

    unevaluated vocabulary

--- lib/vocabularies/errors.ts
+++ lib/vocabularies/errors.ts
@@ -2,8 +2,8 @@ import type {TypeError} from "../compile/validate/dataType"
 import type {ApplicatorKeywordError} from "./applicator"
 import type {ValidationKeywordError} from "./validation"
 import type {FormatError} from "./format/format"
-import type {UnevaluatedPropertiesError} from "./applicator/unevaluatedProperties"
-import type {UnevaluatedItemsError} from "./applicator/unevaluatedItems"
+import type {UnevaluatedPropertiesError} from "./unevaluated/unevaluatedProperties"
+import type {UnevaluatedItemsError} from "./unevaluated/unevaluatedItems"
 import type {DependentRequiredError} from "./validation/dependentRequired"
 
 export type DefinedError =
