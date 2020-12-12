26db61421ab247d0c4cd4216a84f5172e3c48ec0
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-12-12 10:01:30 +0000

    reduced failing schema for #1344

--- spec/issues/1344_fhir_standalone.spec.ts
+++ spec/issues/1344_fhir_standalone.spec.ts
@@ -1,10 +1,12 @@
 import _Ajv from "../ajv"
-import fhirSchema = require("./1344_fhir_schema.json")
+import fhirSchema = require("./1344_schema.json")
 import standaloneCode from "../../dist/standalone"
 import requireFromString = require("require-from-string")
 import assert = require("assert")
 
-describe.only("issue #1344: FHIR schema", () => {
+describe.only("issue #1344: FHIR schema", function () {
+  this.timeout(10000)
+
   it("should compile to standalone code", () => {
     const ajv = new _Ajv({
       strict: false,
@@ -13,11 +15,11 @@ describe.only("issue #1344: FHIR schema", () => {
     ajv.addSchema(fhirSchema)
     const validate = ajv.getSchema("http://hl7.org/fhir/json-schema/4.0#/definitions/Questionnaire")
     assert(typeof validate == "function")
-    assert.strictEqual(validate({}), false)
+    assert.strictEqual(validate({}), true)
 
     const moduleCode = standaloneCode(ajv, validate)
     const standaloneValidate = requireFromString(moduleCode)
     assert(typeof standaloneValidate == "function")
-    assert.strictEqual(standaloneValidate({}), false)
+    assert.strictEqual(standaloneValidate({}), true)
   })
 })
