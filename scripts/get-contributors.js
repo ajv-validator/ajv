83795962c1e72041e083825a002993011bbd3e20
Efim Poberezkin <8711996+efim-poberezkin@users.noreply.github.com>
2021-03-13 14:41:40 +0400

    fix style

--- scripts/get-contributors.js
+++ scripts/get-contributors.js
@@ -13,46 +13,47 @@ const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
 const SIZE = 64;
 
 async function main() {
-	const contributors = [];
-	let page = 1;
+  const contributors = [];
+  let page = 1;
 
-	while (true) {
-		const res = await fetch(`${base}?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&per_page=100&page=${page++}`);
-		const list = await res.json();
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const res = await fetch(`${base}?client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}&per_page=100&page=${page++}`);
+    const list = await res.json();
 
-		if (list.length === 0) break;
+    if (list.length === 0) break;
 
-		contributors.push(...list);
-	}
+    contributors.push(...list);
+  }
 
-	const bots = ['dependabot-preview[bot]', 'greenkeeper[bot]', 'greenkeeperio-bot']
+  const bots = ['dependabot-preview[bot]', 'greenkeeper[bot]', 'greenkeeperio-bot']
 
-	const authors = contributors
-		.filter(a => !bots.includes(a.login))
-		.sort((a, b) => b.contributions - a.contributions);
+  const authors = contributors
+    .filter(a => !bots.includes(a.login))
+    .sort((a, b) => b.contributions - a.contributions);
 
-	const sprite = new Jimp(SIZE * authors.length, SIZE);
+  const sprite = new Jimp(SIZE * authors.length, SIZE);
 
-	for (let i = 0; i < authors.length; i += 1) {
-		const author = authors[i];
-		console.log(`${i + 1} / ${authors.length}: ${author.login}`);
+  for (let i = 0; i < authors.length; i += 1) {
+    const author = authors[i];
+    console.log(`${i + 1} / ${authors.length}: ${author.login}`);
 
-		const image_data = await fetch(author.avatar_url);
-		const buffer = await image_data.arrayBuffer();
+    const image_data = await fetch(author.avatar_url);
+    const buffer = await image_data.arrayBuffer();
 
-		const image = await Jimp.read(buffer);
-		image.resize(SIZE, SIZE);
+    const image = await Jimp.read(buffer);
+    image.resize(SIZE, SIZE);
 
-		sprite.composite(image, i * SIZE, 0);
-	}
+    sprite.composite(image, i * SIZE, 0);
+  }
 
-	await sprite.quality(80).write(`../docs/contributors/contributors.jpg`);
-	// TODO: Optimizing the contributors.jpg image should probably get automated as well
-	console.log('remember to additionally optimize the resulting contributors.jpg image file via e.g. https://squoosh.app ');
+  await sprite.quality(80).write(`../docs/contributors/contributors.jpg`);
+  // TODO: Optimizing the contributors.jpg image should probably get automated as well
+  console.log('remember to additionally optimize the resulting contributors.jpg image file via e.g. https://squoosh.app ');
 
-	const str = `[\n\t${authors.map(a => `'${a.login}'`).join(',\n\t')}\n]`;
+  const str = `[\n\t${authors.map(a => `'${a.login}'`).join(',\n\t')}\n]`;
 
-	fs.writeFileSync(`../docs/contributors/_contributors.js`, `export default ${str};`);
+  fs.writeFileSync(`../docs/contributors/_contributors.js`, `export default ${str};`);
 }
 
 main();
