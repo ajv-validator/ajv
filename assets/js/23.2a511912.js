a06f78d61e667587a0232131697b698e6120906d
GitHub workflow <2769109+epoberezkin@users.noreply.github.com>
2021-03-27 10:59:52 +0000

    updated by ajv workflow https://github.com/ajv-validator/ajv/actions/runs/692661438

---
+++ assets/js/23.2a511912.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{348:function(t,n,s){},428:function(t,n,s){"use strict";s(348)},493:function(t,n,s){"use strict";s.r(n);s(428);var e=s(44),i=Object(e.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"testimonials"},[this._t("default")],2)}),[],!1,null,"729b0721",null);n.default=i.exports}}]);
