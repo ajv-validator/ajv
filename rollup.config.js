b7809449d9f829c061880a0d48a8dc3779d6ef66
Rouven Weßling <rouven@contentful.com>
2021-03-30 16:40:39 +0200

    Create browser bundles with rollup instead of browserify

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import commonjs from "@rollup/plugin-commonjs"
+import {nodeResolve} from "@rollup/plugin-node-resolve"
+import json from "@rollup/plugin-json"
+import typescript from "@rollup/plugin-typescript"
+import {terser} from "rollup-plugin-terser"
+
+function createBundleConfig(sourceFile, outFile, globalName) {
+  return {
+    input: `./lib/${sourceFile}.ts`,
+    output: [
+      {
+        file: `./bundle/${outFile}.bundle.js`,
+        format: "umd",
+        name: globalName,
+      },
+      {
+        file: `./bundle/${outFile}.min.js`,
+        format: "umd",
+        name: globalName,
+        sourcemap: true,
+        plugins: [terser()],
+      },
+    ],
+    plugins: [commonjs(), nodeResolve(), json(), typescript()],
+  }
+}
+
+export default [
+  createBundleConfig("ajv", "ajv7", "av7"),
+  createBundleConfig("2019", "ajv2019", "ajv2019"),
+  createBundleConfig("jtd", "ajvJTD", "ajvJTD"),
+]
