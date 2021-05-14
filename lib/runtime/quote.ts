2ec13c88186a311b625c141d5f76641d9ab1edce
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-05-14 20:04:56 +0100

    fix prettier layout, update node versions

--- lib/runtime/quote.ts
+++ lib/runtime/quote.ts
@@ -1,5 +1,6 @@
-// eslint-disable-next-line no-control-regex, no-misleading-character-class
-const rxEscapable = /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g
+const rxEscapable =
+  // eslint-disable-next-line no-control-regex, no-misleading-character-class
+  /[\\"\u0000-\u001f\u007f-\u009f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g
 
 const escaped: {[K in string]?: string} = {
   "\b": "\\b",
