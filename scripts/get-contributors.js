4b211a4d6fe5ba7ebaed899bd0aedc806679a943
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2021-11-03 20:34:14 +0000

    debugging contributors

--- scripts/get-contributors.js
+++ scripts/get-contributors.js
@@ -25,6 +25,7 @@ async function main() {
       headers: {Authorization: `token ${GH_TOKEN_PUBLIC}`},
     })
     const list = await res.json()
+    console.log(list)
     if (list.length === 0) break
     contributors.push(...list)
   }
