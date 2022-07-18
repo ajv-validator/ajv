24fb12e733bbca2bf6ad44e85753ba59904251bb
romdej <romain@romdej.dev>
2022-07-18 17:04:40 +0200

    fix: added support for asynchronous keywords in JTD schemas (#2029)
    The main changes are:
    
    - The $async keyword is now added only if the JTD option is not present, otherwise a new async keyword is added.
    - The SchemaEnv.$async property is now set with the value of $async or metadata.async properties.
    - The SchemaObject and AsyncSchema types now includes the metadata.async property.

--- spec/ajv.ts
+++ spec/ajv.ts
@@ -1,6 +1,12 @@
 import type Ajv from "../dist/core"
+import type AjvJtd from "../dist/jtd"
+
 const AjvClass: typeof Ajv = typeof window == "object" ? (window as any).ajv7 : require("" + "..")
 
 export default AjvClass
 module.exports = AjvClass
 module.exports.default = AjvClass
+
+export const AjvJtdClass: typeof AjvJtd =
+  typeof window == "object" ? (window as any).ajvJTD : require("../dist/jtd")
+module.exports.AjvJtdClass = AjvJtdClass
