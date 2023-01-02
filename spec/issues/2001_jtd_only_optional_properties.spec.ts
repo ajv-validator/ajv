1751b923ca4caf0744812a91cde55b8e6ac916c6
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-01-02 21:46:30 +0000

    fix test

--- spec/issues/2001_jtd_only_optional_properties.spec.ts
+++ spec/issues/2001_jtd_only_optional_properties.spec.ts
@@ -14,14 +14,11 @@ describe("schema with optional/additional properties only", () => {
       additionalProperties: true,
     }
     const serialize = ajv.compileSerializer(schema)
+    const test = (data, json) => assert.strictEqual(serialize(data), json)
     test({prop0: 0, prop1: 1, prop2: 2}, '{"prop0":0,"prop1":1,"prop2":2}')
     test({prop1: 1, prop2: 2}, '{"prop1":1,"prop2":2}')
     test({prop0: 0, prop1: 1, prop2: 2, foo: "bar"}, '{"prop0":0,"prop1":1,"prop2":2,"foo":"bar"}')
     test({prop1: 1, prop2: 2, foo: "bar"}, '{"prop1":1,"prop2":2,"foo":"bar"}')
     test({foo: "bar"}, '{"foo":"bar"}')
-
-    function test(data: object, json: string) {
-      assert.strictEqual(serialize(data), json)
-    }
   })
 })
