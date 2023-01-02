ffbc2099d459ba979a38573ee6a29003966344c7
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2023-01-02 23:37:40 +0000

    narrow number to integer correctly

--- spec/issues/1935_integer_narrowing_subschema.spec.ts
+++ spec/issues/1935_integer_narrowing_subschema.spec.ts
@@ -1,103 +1,111 @@
 import _Ajv from "../ajv"
 import Ajv from "ajv"
+import * as assert from "assert"
 
-describe("issue 1935: integer valid type in number sub-schema", () => {
+describe("integer valid type in number sub-schema (issue #1935)", () => {
   let ajv: Ajv
   before(() => {
     ajv = new _Ajv({strict: true})
   })
 
-  it("should allow integer in `if`", () => {
-    ajv.compile({
-      type: "number",
-      if: {
-        type: "integer",
-        maximum: 5,
-      },
-      else: {
-        minimum: 10,
-      },
-    })
-  })
+  it("should allow integer in `if`", () =>
+    assert.doesNotThrow(() =>
+      ajv.compile({
+        type: "number",
+        if: {
+          type: "integer",
+          maximum: 5,
+        },
+        else: {
+          minimum: 10,
+        },
+      })
+    ))
 
-  it("should allow integer in `then`", () => {
-    ajv.compile({
-      type: "number",
-      if: {
-        multipleOf: 2,
-      },
-      then: {
-        type: "integer",
-        minimum: 10,
-      },
-    })
-  })
-
-  it("should allow integer in `else`", () => {
-    ajv.compile({
-      type: "number",
-      if: {
-        maximum: 5,
-      },
-      else: {
-        type: "integer",
-        minimum: 10,
-      },
-    })
-  })
-
-  it("should allow integer in `allOf`", () => {
-    ajv.compile({
-      type: "number",
-      allOf: [
-        {
+  it("should allow integer in `then`", () =>
+    assert.doesNotThrow(() =>
+      ajv.compile({
+        type: "number",
+        if: {
+          multipleOf: 2,
+        },
+        then: {
           type: "integer",
           minimum: 10,
         },
-        {
-          multipleOf: 2,
-        },
-      ],
-    })
-  })
+      })
+    ))
 
-  it("should allow integer in `oneOf`", () => {
-    ajv.compile({
-      type: "number",
-      oneOf: [
-        {
+  it("should allow integer in `else`", () =>
+    assert.doesNotThrow(() =>
+      ajv.compile({
+        type: "number",
+        if: {
+          maximum: 5,
+        },
+        else: {
           type: "integer",
           minimum: 10,
         },
-        {
-          multipleOf: 2,
-        },
-      ],
-    })
-  })
+      })
+    ))
 
-  it("should allow integer in `anyOf`", () => {
-    ajv.compile({
-      type: "number",
-      oneOf: [
-        {
+  it("should allow integer in `allOf`", () =>
+    assert.doesNotThrow(() =>
+      ajv.compile({
+        type: "number",
+        allOf: [
+          {
+            type: "integer",
+            minimum: 10,
+          },
+          {
+            multipleOf: 2,
+          },
+        ],
+      })
+    ))
+
+  it("should allow integer in `oneOf`", () =>
+    assert.doesNotThrow(() =>
+      ajv.compile({
+        type: "number",
+        oneOf: [
+          {
+            type: "integer",
+            minimum: 10,
+          },
+          {
+            multipleOf: 2,
+          },
+        ],
+      })
+    ))
+
+  it("should allow integer in `anyOf`", () =>
+    assert.doesNotThrow(() =>
+      ajv.compile({
+        type: "number",
+        oneOf: [
+          {
+            type: "integer",
+            minimum: 10,
+          },
+          {
+            multipleOf: 2,
+          },
+        ],
+      })
+    ))
+
+  it("should allow integer in `not`", () =>
+    assert.doesNotThrow(() =>
+      ajv.compile({
+        type: "number",
+        not: {
           type: "integer",
           minimum: 10,
         },
-        {
-          multipleOf: 2,
-        },
-      ],
-    })
-  })
-
-  it("should allow integer in `not`", () => {
-    ajv.compile({
-      type: "number",
-      not: {
-        type: "integer",
-        minimum: 10,
-      },
-    })
-  })
+      })
+    ))
 })
