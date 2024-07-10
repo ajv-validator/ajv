fb5803eb227034883452eb1e560140bca5af9aa2
Gürgün Dayıoğlu <hey@gurgun.day>
2024-07-11 01:28:50 +0300

    use dequal

--- lib/runtime/equal.ts
+++ lib/runtime/equal.ts
@@ -1,5 +1,5 @@
 // https://github.com/ajv-validator/ajv/issues/889
-import * as equal from "fast-deep-equal"
+import { dequal as equal } from "dequal"
 
 type Equal = typeof equal & {code: string}
 ;(equal as Equal).code = 'require("ajv/dist/runtime/equal").default'
