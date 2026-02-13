7595145cbb5b582f46da5afab5135b56337dd35c
Lucas Akira Uehara <80917717@telefonicati.onmicrosoft.com>
2026-02-13 17:58:35 -0300

    fix(pattern): address CVE-2025-69873 by implementing safeguards against ReDoS attacks in pattern validation

--- lib/vocabularies/validation/pattern.ts
+++ lib/vocabularies/validation/pattern.ts
@@ -1,6 +1,7 @@
 import type {CodeKeywordDefinition, ErrorObject, KeywordErrorDefinition} from "../../types"
 import type {KeywordCxt} from "../../compile/validate"
 import {usePattern} from "../code"
+import {useFunc} from "../../compile/util"
 import {_, str} from "../../compile/codegen"
 
 export type PatternError = ErrorObject<"pattern", {pattern: string}, string | {$data: string}>
@@ -18,10 +19,21 @@ const def: CodeKeywordDefinition = {
   error,
   code(cxt: KeywordCxt) {
     const {data, $data, schema, schemaCode, it} = cxt
-    // TODO regexp should be wrapped in try/catchs
     const u = it.opts.unicodeRegExp ? "u" : ""
-    const regExp = $data ? _`(new RegExp(${schemaCode}, ${u}))` : usePattern(cxt, schema)
-    cxt.fail$data(_`!${regExp}.test(${data})`)
+    if ($data) {
+      const {gen} = cxt
+      const {regExp} = it.opts.code
+      const regExpCode = regExp.code === "new RegExp" ? _`new RegExp` : useFunc(gen, regExp)
+      const valid = gen.let("valid")
+      gen.try(
+        () => gen.assign(valid, _`${regExpCode}(${schemaCode}, ${u}).test(${data})`),
+        (_e) => gen.assign(valid, false)
+      )
+      cxt.fail$data(_`!${valid}`)
+    } else {
+      const regExp = usePattern(cxt, schema)
+      cxt.fail$data(_`!${regExp}.test(${data})`)
+    }
   },
 }
 
