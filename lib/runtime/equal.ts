e6c7e616ed589f9f028ddf9a670387afa9739b19
xxiaoxiong <xxiaoxiong@users.noreply.github.com>
2026-05-25 15:25:09 +0800

    fix: guard equal() against non-function toString/valueOf properties

--- lib/runtime/equal.ts
+++ lib/runtime/equal.ts
@@ -1,7 +1,28 @@
 // https://github.com/ajv-validator/ajv/issues/889
 import * as equal from "fast-deep-equal"
 
-type Equal = typeof equal & {code: string}
-;(equal as Equal).code = 'require("ajv/dist/runtime/equal").default'
+// Guard against TypeError when objects have non-function toString/valueOf properties
+// (see https://github.com/ajv-validator/ajv/issues/2624)
+function safeEqual(a: unknown, b: unknown): boolean {
+  if (a && typeof a === "object") {
+    safeGuard(a as Record<string, unknown>)
+  }
+  if (b && typeof b === "object") {
+    safeGuard(b as Record<string, unknown>)
+  }
+  return equal(a, b)
+}
 
-export default equal as Equal
+function safeGuard(obj: Record<string, unknown>) {
+  if (typeof obj.toString !== "function") {
+    obj.toString = Object.prototype.toString as () => string
+  }
+  if (typeof obj.valueOf !== "function") {
+    obj.valueOf = Object.prototype.valueOf as () => unknown
+  }
+}
+
+type Equal = typeof equal & {code: string}
+;(safeEqual as Equal).code = 'require("ajv/dist/runtime/equal").default'
+
+export default safeEqual as Equal
